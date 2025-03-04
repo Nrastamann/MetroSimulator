643e8b3189dc4f27722a13b9d46f3a5c1b1ca799
tush00nka <endline00@ya.ru>
2025-03-04 16:34:47 +0300

    circles are drawn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use bevy::prelude::*;
 
+mod camera;
+use camera::CameraPlugin;
+
+mod map_generation;
+use map_generation::MapGenerationPlugin;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .run(); 
+        .add_plugins(CameraPlugin)
+        .add_plugins(MapGenerationPlugin)
+        .run();
 }
---
+++ src/map_generation.rs
@@ -0,0 +1,31 @@
+use bevy::prelude::*;
+
+pub struct MapGenerationPlugin;
+
+impl Plugin for MapGenerationPlugin{
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_map);
+    }
+}
+
+fn spawn_map(
+    mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    for i in 0..5 {
+        commands.spawn((
+            Mesh2d(meshes.add(Circle::new(25.))),
+            MeshMaterial2d(materials.add(Color::hsl(20., 0.5, 0.5))),
+            Transform::from_xyz(i as f32 * 100., (i as f32).powi(2) * 20., 0.)
+        ));
+    }
+
+    for i in -5..0 {
+        commands.spawn((
+            Mesh2d(meshes.add(Circle::new(25.))),
+            MeshMaterial2d(materials.add(Color::hsl(20., 0.5, 0.5))),
+            Transform::from_xyz(i as f32 * 100., -(i as f32).powi(2) * 20., 0.)
+        ));
+    }
+}
