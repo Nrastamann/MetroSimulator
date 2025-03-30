c0c14ee22096464bd0c6798b86a98b2de7b7ce94
Nrastamann <nicita351@Yandex.ru>
2025-03-30 20:49:48 +0300

    Feature(right-click menu) Added menu on right click with some info about station

--- src/station_ui.rs
+++ src/station_ui.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_lunex::{Ab, Rl, Rw, UiFetchFromCamera, UiLayout, UiLayoutRoot};
 
 use crate::{
     cursor::CursorPosition, loading_screen::METRO_BLUE_COLOR, metro::Metro, station::Station,
@@ -17,7 +16,7 @@ impl Plugin for StationUIPlugin {
 }
 #[derive(Event)]
 pub struct SpawnPopupEvent {
-    mouse_pos: CursorPosition,
+    mouse_pos: Vec2,
     station: Station,
 }
 
@@ -25,16 +24,168 @@ pub struct SpawnPopupEvent {
 pub struct PopupMenu;
 
 impl PopupMenu {
-    fn draw_popup(mut draw_info: EventReader<SpawnPopupEvent>, mut commands: Commands) {
+    fn draw_popup(
+        mut draw_info: EventReader<SpawnPopupEvent>,
+        mut commands: Commands,
+        mut popup_query: Query<Entity, With<PopupMenu>>,
+        query_window: Query<&Window>,
+        mut asset_server: Res<AssetServer>,
+    ) {
+        let window = query_window.single();
         for ev in draw_info.read() {
+            for entity in popup_query.iter_mut() {
+                commands.entity(entity).despawn_recursive();
+            }
+
+            let rel_pos = (
+                (ev.mouse_pos.x / window.width()).abs(),
+                (ev.mouse_pos.y / window.height()).abs(),
+            );
+
             commands
-                .spawn((UiLayoutRoot::new_2d(), UiFetchFromCamera::<0>, PopupMenu))
+                .spawn((
+                    Node {
+                        position_type: PositionType::Absolute,
+                        top: Val::Percent(rel_pos.1 * 100.),
+                        left: Val::Percent(rel_pos.0 * 100.),
+
+                        width: Val::Percent(35.0),
+                        height: Val::Percent(20.0),
+                        ..default()
+                    },
+                    BackgroundColor(Color::BLACK),
+                    PopupMenu,
+                ))
                 .with_children(|ui| {
+                    //NAME ROOT NODE
                     ui.spawn((
-                        Name::new("Station Menu"),
-                        UiLayout::window().pos((ev.station.position.x,ev.station.position.y)).size(Rl((10., 20.))).pack(),
-                        Sprite::from_color(METRO_BLUE_COLOR, (1., 1.).into()),
+                        Node {
+                            position_type: PositionType::Absolute,
+                            top: Val::Percent(0.),
+                            left: Val::Percent(0.),
+
+                            width: Val::Percent(100.),
+                            height: Val::Percent(30.),
+
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        BackgroundColor(METRO_BLUE_COLOR),
+                    )).with_children(|ui|{
+                        ui.spawn((
+                           Text::new(//ev.station.name
+                            "STATION NAME"
+                           ), 
+                           TextFont{
+                            font: asset_server.load("fonts/Metro.ttf"),
+                            font_size: 18.,
+                            ..default()
+                           },
+                           TextColor(Color::WHITE),
+                        ));
+                    });
+                    
+                    //BORDER
+                    
+                    ui.spawn((
+                        Node{
+                            position_type: PositionType::Absolute,
+                            top: Val::Percent(30.),
+                            left: Val::Percent(0.),
+
+                            width: Val::Percent(100.),
+                            height: Val::Percent(1.),
+
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        BackgroundColor(Color::BLACK),
                     ));
+
+                    //STATS ROOT NODE
+                    
+                    ui.spawn((
+                        Node {
+                            position_type: PositionType::Absolute,
+                            top: Val::Percent(31.),
+                            left: Val::Percent(0.),
+
+                            width: Val::Percent(49.5),
+                            height: Val::Percent(69.),
+
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        BackgroundColor(METRO_BLUE_COLOR),
+                    )).with_children(|ui|{
+                        ui.spawn((
+                           Text::new(//ev.station.name
+                            "STATS SECTION"
+                           ), 
+                           TextFont{
+                            font: asset_server.load("fonts/Metro.ttf"),
+                            font_size: 18.,
+                            ..default()
+                           },
+                           TextColor(Color::WHITE),
+                        ));
+                    });
+
+                    //BORDER NODE
+
+                    ui.spawn((
+                        Node{
+                            position_type: PositionType::Absolute,
+                            top: Val::Percent(31.),
+                            left: Val::Percent(49.5),
+
+                            width: Val::Percent(1.),
+                            height: Val::Percent(69.),
+
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        BackgroundColor(Color::BLACK),
+                    ));
+                    
+                    //BUTTON ROOT NODE
+
+                    ui.spawn((
+                        Node {
+                            position_type: PositionType::Absolute,
+                            top: Val::Percent(31.),
+                            left: Val::Percent(50.5),
+
+                            width: Val::Percent(49.5),
+                            height: Val::Percent(69.),
+
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        BackgroundColor(METRO_BLUE_COLOR),
+                    )).with_children(|ui|{
+                        ui.spawn((
+                           Text::new(//ev.station.name
+                            "LINES SECTION"
+                           ), 
+                           TextFont{
+                            font: asset_server.load("fonts/Metro.ttf"),
+                            font_size: 18.,
+                            ..default()
+                           },
+                           TextColor(Color::WHITE),
+                        ));
+                    });
                 });
         }
     }
@@ -43,17 +194,22 @@ impl PopupMenu {
 fn draw_menu(
     stations: Query<&Station>,
     mut draw_popup: EventWriter<SpawnPopupEvent>,
-    cursor_position: Res<CursorPosition>,
     mouse: Res<ButtonInput<MouseButton>>,
+    query_window: Query<&Window>,
 ) {
     if mouse.just_pressed(MouseButton::Right) {
         // начинаем строить, определяем, будет это продолжение старой ветки или создание новой
         for station in stations.iter() {
             if station.selected {
-                draw_popup.send(SpawnPopupEvent {
-                    mouse_pos: cursor_position.clone(),
-                    station: *station,
-                });
+                let window = query_window.single();
+                if let Some(cursor_pos) = window.cursor_position() {
+                    draw_popup.send(SpawnPopupEvent {
+                        mouse_pos: cursor_pos.clone(),
+                        station: *station,
+                    });
+                } else {
+                    panic!("Error: Cursor is not founded");
+                }
             }
         }
     }
