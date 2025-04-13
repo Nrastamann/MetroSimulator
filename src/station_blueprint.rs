30c1ebbb9798ab37e971b057ff1e940b3edff28e
Nrastamann <nicita351@Yandex.ru>
2025-04-13 23:12:53 +0300

    Refactor(Removed Builder) Removed builder, need to fix to work, also need to change some ui.

--- src/station_blueprint.rs
+++ src/station_blueprint.rs
@@ -1,10 +1,19 @@
 use bevy::prelude::*;
 
-use crate::{cursor::CursorPosition, GameState, DISTRICT_CELL_SIZE};
+use crate::{cursor::CursorPosition, station::StartBuildingEvent, GameState, DISTRICT_CELL_SIZE};
+#[derive(Copy,Clone)]
+pub enum Direction {
+    Forward,
+    Backwards,
+}
 
 #[derive(Component)]
 pub struct StationBlueprint {
-    material: Handle<ColorMaterial>,
+    pub material: Handle<ColorMaterial>,
+    pub connection: (i32, i32),
+    pub direction: Direction,
+    pub line_to_attach: usize, //if we want to add new line, send -1
+    pub can_build: bool
 }
 
 pub struct StationBlueprintPlugin;
@@ -13,7 +22,10 @@ impl Plugin for StationBlueprintPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<SetBlueprintColorEvent>();
         app.add_systems(Startup, init_blueprint);
-        app.add_systems(Update, (stick_to_mouse, toggle_station_blueprint).run_if(in_state(GameState::InGame)));
+        app.add_systems(
+            Update,
+            (stick_to_mouse, toggle_station_blueprint, start_building).run_if(in_state(GameState::InGame)),
+        );
     }
 }
 
@@ -26,13 +38,20 @@ fn init_blueprint(
     commands.spawn((
         Mesh2d(meshes.add(Circle::new(25.))),
         MeshMaterial2d(material.clone()),
-        StationBlueprint { material },
+        StationBlueprint {
+            material: material,
+            connection: (0, 0),
+            direction: Direction::Forward,
+            line_to_attach: usize::MAX,
+            can_build: true,
+        },
+        Visibility::Hidden,
     ));
 }
 
 fn stick_to_mouse(
     mut q_blueprint: Query<&mut Transform, With<StationBlueprint>>,
-    cursor_position: Res<CursorPosition>
+    cursor_position: Res<CursorPosition>,
 ) {
     for mut blueprint_transform in q_blueprint.iter_mut() {
         blueprint_transform.translation = 
@@ -49,14 +68,31 @@ pub struct SetBlueprintColorEvent(pub Color);
 
 fn toggle_station_blueprint(
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut q_blueprint: Query<&StationBlueprint>,
-    mut ev_toggle: EventReader<SetBlueprintColorEvent>
+    mut q_blueprint: Query<(&mut Visibility, &StationBlueprint)>,
+    mut ev_toggle: EventReader<SetBlueprintColorEvent>,
 ) {
     for ev in ev_toggle.read() {
-        for blueprint in q_blueprint.iter_mut() {
+        for (mut vision, blueprint) in q_blueprint.iter_mut() {
             if let Some(material) = materials.get_mut(&blueprint.material) {
                 material.color = ev.0;
             }
         }
+    }
+}
+
+fn start_building(
+    mut ev_set_blueprint: EventReader<StartBuildingEvent>,
+    mut blueprint_q: Query<(&mut StationBlueprint, &mut Visibility)>,
+) {
+    for ev in ev_set_blueprint.read() {
+        let Ok((mut blueprint, mut vision)) = blueprint_q.get_single_mut() else{
+            panic!("NO BLUEPRINT");
+        };
+        
+        blueprint.connection = ev.connection;
+        blueprint.direction = ev.direction;
+        blueprint.line_to_attach = ev.line_to_attach;
+        
+        *vision = Visibility::Visible;
     }
 }
