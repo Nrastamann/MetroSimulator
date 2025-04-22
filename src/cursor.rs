52fd7b47a29d21f828f6836837bddaf82b6b0421
tush00nka <endline00@ya.ru>
2025-04-22 23:11:56 +0300

    constants and formatting

--- src/cursor.rs
+++ src/cursor.rs
@@ -14,18 +14,21 @@ pub struct CursorPosition(pub Vec2);
 
 impl CursorPosition {
     pub fn as_tuple(&self) -> (i32, i32) {
-        (self.0.x.floor() as i32,
-         self.0.y.floor() as i32)
+        (self.0.x.floor() as i32, self.0.y.floor() as i32)
     }
 }
 
 fn update_cursor_position(
-    window: Single<&Window, With<PrimaryWindow>>, 
+    window: Single<&Window, With<PrimaryWindow>>,
     q_camera: Query<(&Camera, &GlobalTransform)>,
     mut position: ResMut<CursorPosition>,
 ) {
-    let Ok((camera, camera_transform)) = q_camera.get_single() else { return };
-    let Some(cursor) = window.cursor_position() else { return };
+    let Ok((camera, camera_transform)) = q_camera.get_single() else {
+        return;
+    };
+    let Some(cursor) = window.cursor_position() else {
+        return;
+    };
     if let Ok(mouse_position) = camera.viewport_to_world_2d(camera_transform, cursor) {
         position.0 = mouse_position;
     }
